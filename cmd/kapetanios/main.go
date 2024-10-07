2cce846456e9b6839475fd11504a049735615910
shishir9159 <nahidhasan43@iut-dhaka.edu>
2024-10-07 11:50:46 +0600

    factor over form - fiber

--- cmd/kapetanios/main.go
+++ cmd/kapetanios/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/healthcheck"
 	"github.com/gofiber/swagger"
 	"net/http"
 )
@@ -22,12 +22,12 @@ func certRenewal(c *fiber.Ctx) error {
 
 func setupRoutes(app *fiber.App) {
 
-	api := app.Group("/cert", logger.New())
+	//api := app.Group("/cert", logger.New())
 	//minorUpgrade := app.Group("minor-upgrade")
 
 	app.Get("/swagger", swagger.HandlerDefault)
 	app.Get("/renewal", certRenewal)
-	api.Group("")
+	//api.Group("")
 	//	.SetupRoutes(cert)
 	//	.SetupRoutes(minorUpgrade)
 
@@ -42,11 +42,22 @@ func main() {
 
 	app := fiber.New()
 
-	app.Use(logger.New())
+	//app.Use(logger.New())
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "*",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 		AllowMethods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+	}))
+
+	app.Use(healthcheck.New(healthcheck.Config{
+		LivenessProbe: func(c *fiber.Ctx) bool {
+			return true
+		},
+		LivenessEndpoint: "/healthz",
+		ReadinessProbe: func(c *fiber.Ctx) bool {
+			return true
+		},
+		ReadinessEndpoint: "/healthz",
 	}))
 
 	// setup routes
