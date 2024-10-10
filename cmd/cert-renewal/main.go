48a40cca03b976ed9741c56541b8b387cd923f43
shishir9159 <nahidhasan43@iut-dhaka.edu>
2024-10-10 22:01:17 +0600

    utilizing file descriptor return back to container

--- cmd/cert-renewal/main.go
+++ cmd/cert-renewal/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"context"
+	"fmt"
 	"go.uber.org/zap"
-	"os/exec"
 )
 
 var (
@@ -19,12 +18,11 @@ type Controller struct {
 func main() {
 
 	logger, err := zap.NewProduction()
-	defer func(logger *zap.Logger) {
-		er := logger.Sync()
-		if er != nil {
+	if err != nil {
+		fmt.Println(err)
+	}
 
-		}
-	}(logger)
+	//zap.ReplaceGlobals(logger)
 
 	// replace zap with zerolog
 
@@ -33,62 +31,56 @@ func main() {
 		log: logger,
 	}
 
+	defer func(logger *zap.Logger) {
+		er := logger.Sync()
+		if er != nil {
+			c.log.Error("failed to close logger", zap.Error(er))
+		}
+	}(logger)
+
 	GrpcClient(c.log)
 
-	var outb, errb bytes.Buffer
-	cmd := exec.Command("/bin/bash", "-c", "ls")
-	cmd.Stdout = &outb
-	cmd.Stderr = &errb
-
-	//zap.ReplaceGlobals(logger)
-
-	//logger.Info("failed to fetch URL",
-	//	// Structured context as strongly typed Field values.
-	//	//zap.String("url", url),
-	//	zap.Int("attempt", 3),
-	//	zap.Duration("backoff", time.Second),
-	//)
-
-	//client, err := orchestration.NewClient()
+	//var outb, errb bytes.Buffer
+	//cmd := exec.Command("/bin/bash", "-c", "ls")
+	//cmd.Stdout = &outb
+	//cmd.Stderr = &errb
+	//
+	//
+	//
+	//GrpcClient(c.log)
+	//
+	////	step 1. Backup directories
+	//err = BackupCertificatesKubeConfigs(c, backupCount)
 	//if err != nil {
-	//	logger.Fatal("error creating Kubernetes client: ",
+	//	c.log.Error("failed to backup certificates and kubeConfigs",
 	//		zap.Error(err))
 	//}
-
-	GrpcClient(c.log)
-
-	//	step 1. Backup directories
-	err = BackupCertificatesKubeConfigs(c, backupCount)
-	if err != nil {
-		c.log.Error("failed to backup certificates and kubeConfigs",
-			zap.Error(err))
-	}
-
-	//	step 2. Kubeadm certs renew all
-
-	//    cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
-	err = cmd.Run()
-	c.log.Info("ls after step 1 chroot",
-		zap.String("output", outb.String()),
-		zap.String("err", errb.String()))
-
-	err = Renew(c)
-	if err != nil {
-		c.log.Error("failed to renew certificates and kubeConfigs",
-			zap.Error(err))
-	}
-
-	err = cmd.Run()
-	c.log.Info("ls -after renew step 2 chroot",
-		zap.String("output", outb.String()),
-		zap.String("err", errb.String()))
-
-	//step 3. Restarting pods to work with the updated certificates
-	err = Restart(c)
-	if err != nil {
-		c.log.Error("failed to restart kubernetes components after certificate renewal",
-			zap.Error(err))
-	}
-
-	GrpcClient(c.log)
+	//
+	////	step 2. Kubeadm certs renew all
+	//
+	////    cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+	//err = cmd.Run()
+	//c.log.Info("ls after step 1 chroot",
+	//	zap.String("output", outb.String()),
+	//	zap.String("err", errb.String()))
+	//
+	//err = Renew(c)
+	//if err != nil {
+	//	c.log.Error("failed to renew certificates and kubeConfigs",
+	//		zap.Error(err))
+	//}
+	//
+	//err = cmd.Run()
+	//c.log.Info("ls -after renew step 2 chroot",
+	//	zap.String("output", outb.String()),
+	//	zap.String("err", errb.String()))
+	//
+	////step 3. Restarting pods to work with the updated certificates
+	//err = Restart(c)
+	//if err != nil {
+	//	c.log.Error("failed to restart kubernetes components after certificate renewal",
+	//		zap.Error(err))
+	//}
+	//
+	//GrpcClient(c.log)
 }
