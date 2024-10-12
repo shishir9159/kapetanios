9abffbc2a25166d70bc6610c4bf460d52953fdce
shishir9159 <nahidhasan43@iut-dhaka.edu>
2024-10-12 08:44:37 +0600

    dockerfile trial and error - 2

--- cmd/cert-renewal/grpc-client.go
+++ cmd/cert-renewal/grpc-client.go
@@ -3,11 +3,10 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
+	pb "github.com/shishir9159/kapetanios/proto"
 	"go.uber.org/zap"
-	"io"
-	"net"
-	"net/http"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"time"
 )
 
@@ -29,109 +28,109 @@ var (
 func GrpcClient(log *zap.Logger) {
 
 	flag.Parse()
-	client := &http.Client{
-		Timeout: time.Second * 5,
-		Transport: &http.Transport{
-			// Avoid: "x509: certificate signed by unknown authority"
-			//TLSClientConfig: &tls.Config{
-			//	InsecureSkipVerify: true,
-			//},
-			// Inspect the network connection type
-			DialContext: func(ctx context.Context, network string, addr string) (net.Conn, error) {
-				return (&net.Dialer{}).DialContext(ctx, "tcp4", addr)
-			},
-
-			//DialContext: (&net.Dialer{
-			//	Control: func(network, address string, c syscall.RawConn) error {
-			//		// Reference: https://golang.org/pkg/net/#Dial
-			//		if network == "tcp4" {
-			//			log.Error("ipv6 error")
-			//		}
-			//		return nil
-			//	},
-			//}).DialContext,
-		},
-	}
-
-	r := &net.Resolver{
-		PreferGo: true,
-		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
-			d := net.Dialer{
-				Timeout: time.Millisecond * time.Duration(10000),
-			}
-			return d.DialContext(ctx, network, "10.96.0.10:53")
-		},
-	}
-
-	ip, err := r.LookupIP(context.Background(), "ip4", "www.google.com")
-	if len(ip) != 0 {
-		log.Info("google address")
-		fmt.Println(ip)
-	}
-
-	if err != nil {
-		log.Error("error google address", zap.Error(err))
-		fmt.Println(ip)
-	}
-
-	ip, err = r.LookupIP(context.Background(), "ip4", "http://hello.default.svc.cluster.local")
-	if len(ip) != 0 {
-		log.Info("hello http address")
-		fmt.Println(ip)
-	}
-
-	if err != nil {
-		log.Error("error hello http address", zap.Error(err))
-		fmt.Println(ip)
-	}
-
-	ip, err = r.LookupIP(context.Background(), "ip4", "hello.default.svc.cluster.local")
-	if len(ip) != 0 {
-		log.Info("hello service address")
-		fmt.Println(ip)
-	}
-
-	if err != nil {
-		log.Error("error hello http address")
-	}
-
-	ip, err = r.LookupIP(context.Background(), "ip4", "kapetanios.default.svc.cluster.local")
-	if len(ip) != 0 {
-		log.Info("hello service address")
-		fmt.Println(ip)
-	}
-
-	if err != nil {
-		log.Error("error kapetanios address")
-	}
-
-	req, err := http.NewRequest("GET", "http://hello.default.svc.cluster.local", nil)
-	if err != nil {
-		log.Error("Failed to connect to hello.default.svc.cluster.local", zap.Error(err))
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Error("error from client request", zap.Error(err))
-	}
-
-	if resp == nil {
-		log.Fatal("response is empty")
-	}
-
-	defer func(Body io.ReadCloser) {
-		er := Body.Close()
-		if er != nil {
-			log.Error("error closing the body")
-		}
-	}(resp.Body)
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Error("Failed to read body", zap.Error(err))
-	}
-
-	log.Info("body", zap.String("body", string(body)))
+	//client := &http.Client{
+	//	Timeout: time.Second * 5,
+	//	Transport: &http.Transport{
+	//		// Avoid: "x509: certificate signed by unknown authority"
+	//		//TLSClientConfig: &tls.Config{
+	//		//	InsecureSkipVerify: true,
+	//		//},
+	//		// Inspect the network connection type
+	//		DialContext: func(ctx context.Context, network string, addr string) (net.Conn, error) {
+	//			return (&net.Dialer{}).DialContext(ctx, "tcp4", addr)
+	//		},
+	//
+	//		//DialContext: (&net.Dialer{
+	//		//	Control: func(network, address string, c syscall.RawConn) error {
+	//		//		// Reference: https://golang.org/pkg/net/#Dial
+	//		//		if network == "tcp4" {
+	//		//			log.Error("ipv6 error")
+	//		//		}
+	//		//		return nil
+	//		//	},
+	//		//}).DialContext,
+	//	},
+	//}
+	//
+	//r := &net.Resolver{
+	//	PreferGo: true,
+	//	Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+	//		d := net.Dialer{
+	//			Timeout: time.Millisecond * time.Duration(10000),
+	//		}
+	//		return d.DialContext(ctx, network, "10.96.0.10:53")
+	//	},
+	//}
+	//
+	//ip, err := r.LookupIP(context.Background(), "ip4", "www.google.com")
+	//if len(ip) != 0 {
+	//	log.Info("google address")
+	//	fmt.Println(ip)
+	//}
+	//
+	//if err != nil {
+	//	log.Error("error google address", zap.Error(err))
+	//	fmt.Println(ip)
+	//}
+	//
+	//ip, err = r.LookupIP(context.Background(), "ip4", "http://hello.default.svc.cluster.local")
+	//if len(ip) != 0 {
+	//	log.Info("hello http address")
+	//	fmt.Println(ip)
+	//}
+	//
+	//if err != nil {
+	//	log.Error("error hello http address", zap.Error(err))
+	//	fmt.Println(ip)
+	//}
+	//
+	//ip, err = r.LookupIP(context.Background(), "ip4", "hello.default.svc.cluster.local")
+	//if len(ip) != 0 {
+	//	log.Info("hello service address")
+	//	fmt.Println(ip)
+	//}
+	//
+	//if err != nil {
+	//	log.Error("error hello http address")
+	//}
+	//
+	//ip, err = r.LookupIP(context.Background(), "ip4", "kapetanios.default.svc.cluster.local")
+	//if len(ip) != 0 {
+	//	log.Info("hello service address")
+	//	fmt.Println(ip)
+	//}
+	//
+	//if err != nil {
+	//	log.Error("error kapetanios address")
+	//}
+	//
+	//req, err := http.NewRequest("GET", "http://hello.default.svc.cluster.local", nil)
+	//if err != nil {
+	//	log.Error("Failed to connect to hello.default.svc.cluster.local", zap.Error(err))
+	//}
+	//
+	//resp, err := client.Do(req)
+	//if err != nil {
+	//	log.Error("error from client request", zap.Error(err))
+	//}
+	//
+	//if resp == nil {
+	//	log.Fatal("response is empty")
+	//}
+	//
+	//defer func(Body io.ReadCloser) {
+	//	er := Body.Close()
+	//	if er != nil {
+	//		log.Error("error closing the body")
+	//	}
+	//}(resp.Body)
+	//
+	//body, err := io.ReadAll(resp.Body)
+	//if err != nil {
+	//	log.Error("Failed to read body", zap.Error(err))
+	//}
+	//
+	//log.Info("body", zap.String("body", string(body)))
 
 	//address, err := net.LookupHost("kapetanios.default.svc.cluster.local")
 	//
@@ -144,29 +143,29 @@ func GrpcClient(log *zap.Logger) {
 	//	addr = flag.String("addr", address[0]+"50051", "the address to connect to")
 	//	addr = &address[0]
 	//}
-	//
-	//// Set up a connection to the server.
-	//conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	//if err != nil {
-	//	log.Error("did not connect", zap.Error(err))
-	//}
-	////grpc.WithDisableServiceConfig()
-	//defer func(conn *grpc.ClientConn) {
-	//	er := conn.Close()
-	//	if er != nil {
-	//		log.Error("failed to close the grpc connection",
-	//			zap.Error(er))
-	//	}
-	//}(conn)
-	//
-	//c := pb.NewRenewalClient(conn)
-	//
-	//// Contact the server and print out its response.
-	//ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	//defer cancel()
-	//r, err := c.StatusUpdate(ctx, &pb.CreateRequest{BackupSuccess: true, RenewalSuccess: true, RestartSuccess: true, Log: "", Err: "s"})
-	//if err != nil {
-	//	log.Error("could not send status update: ", zap.Error(err))
-	//}
-	//log.Error("Status Update", zap.Bool("next step", r.GetProceedNextStep()), zap.Bool("retry", r.GetRetryCurrentStep()))
+
+	// Set up a connection to the server.
+	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Error("did not connect", zap.Error(err))
+	}
+	//grpc.WithDisableServiceConfig()
+	defer func(conn *grpc.ClientConn) {
+		er := conn.Close()
+		if er != nil {
+			log.Error("failed to close the grpc connection",
+				zap.Error(er))
+		}
+	}(conn)
+
+	c := pb.NewRenewalClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.StatusUpdate(ctx, &pb.CreateRequest{BackupSuccess: true, RenewalSuccess: true, RestartSuccess: true, Log: "", Err: "s"})
+	if err != nil {
+		log.Error("could not send status update: ", zap.Error(err))
+	}
+	log.Error("Status Update", zap.Bool("next step", r.GetProceedNextStep()), zap.Bool("retry", r.GetRetryCurrentStep()))
 }
