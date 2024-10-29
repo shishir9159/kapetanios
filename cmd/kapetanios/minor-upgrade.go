8ffea1cae224c824223ca42d8fd06955b6a5c743
shishir9159 <nahidhasan43@iut-dhaka.edu>
2024-10-29 09:44:34 +0600

    targeting minor upgrade for one node with lots of log generation

--- cmd/kapetanios/minor-upgrade.go
+++ cmd/kapetanios/minor-upgrade.go
@@ -192,12 +192,48 @@ func MinorUpgrade(namespace string) {
 		// TODO: If any new Pods tolerate the node.kubernetes.io/unschedulable taint,
 		//  then those Pods might be scheduled to the node you have drained.
 
+		// TODO: force drain boolean
+
 		err = drainAndCordonNode(c, &node)
 		if err != nil {
 			c.log.Error("failed to drain node",
 				zap.String("node name:", node.Name),
 				zap.Error(err))
 		}
+
+		// TODO:
+		//  display the possible errors if force was not
+
+		// TODO:
+		//  if ran successfully
+		//  Warning: deleting Pods that declare no controller: default/dnsutils; ignoring DaemonSet-managed Pods: kube-system/cilium-wnn6z, kube-system/kube-proxy-m8txw, metallb-system/speaker-587dh
+		//evicting pod ingress-nginx/ingress-nginx-admission-create-5pjqv
+		//evicting pod metallb-system/controller-9b6c9f6c9-g2p4z
+		//evicting pod klovercloud/mesh-uat-go-two-6f846bfcf-ztnk5
+		//evicting pod ingress-nginx/ingress-nginx-controller-5dcc84f655-pvdcc
+		//evicting pod klovercloud/mesh-uat-go-one-86776497cf-227g9
+		//evicting pod default/dnsutils
+		//evicting pod default/backend-59b96df495-ghfn2
+		//evicting pod ingress-nginx/ingress-nginx-admission-patch-4qgmn
+		//evicting pod kube-system/coredns-787d4945fb-stk4w
+		//evicting pod kube-system/cilium-operator-fdf6bc9f4-mcx4h
+		//evicting pod default/kapetanios-b5bc457fb-v5vlx
+		//evicting pod default/minions-for-etcd-4mg95
+		//evicting pod kube-system/coredns-787d4945fb-4zl96
+		//pod/ingress-nginx-admission-patch-4qgmn evicted
+		//pod/ingress-nginx-admission-create-5pjqv evicted
+		//pod/controller-9b6c9f6c9-g2p4z evicted
+		//pod/minions-for-etcd-4mg95 evicted
+		//pod/mesh-uat-go-one-86776497cf-227g9 evicted
+		//I1029 03:31:47.241516 4025541 request.go:690] Waited for 1.103702573s due to client-side throttling, not priority and fairness, request: GET:https://10.0.0.3:6443/api/v1/namespaces/klovercloud/pods/mesh-uat-go-two-6f846bfcf-ztnk5
+		//pod/dnsutils evicted
+		//pod/backend-59b96df495-ghfn2 evicted
+		//pod/kapetanios-b5bc457fb-v5vlx evicted
+		//pod/mesh-uat-go-two-6f846bfcf-ztnk5 evicted
+		//pod/cilium-operator-fdf6bc9f4-mcx4h evicted
+		//pod/coredns-787d4945fb-4zl96 evicted
+		//pod/coredns-787d4945fb-stk4w evicted
+		//pod/ingress-nginx-controller-5dcc84f655-pvdcc evicted
 
 		addTaint(&node)
 
@@ -217,7 +253,7 @@ func MinorUpgrade(namespace string) {
 		// TODO: monitor the pod restart after upgrade
 		//  All containers are restarted after upgrade, because the container spec hash value is changed
 		//		just monitor the NODES before creating minion, no need to restart
-		//RestartByLabel(c, map[string]string{"tier": "control-plane"}, node.Name)
+		//  RestartByLabel(c, map[string]string{"tier": "control-plane"}, node.Name)
 
 		minion, er := c.client.Clientset().CoreV1().Pods(namespace).Create(context.Background(), descriptor, metav1.CreateOptions{})
 		if er != nil {
--- cmd/minor-upgrade/scratch.md
+++ cmd/minor-upgrade/scratch.md
@@ -10,13 +10,13 @@ upgrade the worker nodes
 1. kubectl drain node <node-to-drain>
 2. apt-cache madison kubeadm | awk '{ print $3 }'
 3. apt-mark unhold kubeadm && apt-get update && apt-get install -y kubeadm='1.26.5-1.1' && apt-mark hold kubeadm
-4. kubeadm upgrade plan
+4. kubeadm upgrade plan -- not for the worker
 5. kubeadm upgrade apply v1.26.5 --certificate-renewal=false
-6. for other master nodes: kubeadm upgrade node v1.26.5 --certificate-renewal=false
-7. kubectl drain <node-to-drain> --ignore-daemonsets
+6. for other master nodes: kubeadm upgrade node --certificate-renewal=false
+7. kubectl drain <node-to-drain> --ignore-daemonsets -- recommended draining point
 8. apt-mark unhold kubelet kubectl && \
    apt-get update && apt-get install -y kubelet='1.26.5-1.1' kubectl='1.26.5-1.1' && \
    apt-mark hold kubelet kubectl
-
-steps are same for the worker nodes
-
+9. systemctl daemon-reload
+10. systemctl restart kubelet
+11. kubectl uncordon <node-to-drain>
