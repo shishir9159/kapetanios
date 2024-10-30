69ec2539438059c9b647626854b3d48fdf8503d8
shishir9159 <nahidhasan43@iut-dhaka.edu>
2024-10-30 21:42:05 +0600

    targeting minor upgrade for one node with lots of log generation - first attempt

--- cmd/kapetanios/minor-upgrade.go
+++ cmd/kapetanios/minor-upgrade.go
@@ -13,7 +13,7 @@ import (
 
 func drainAndCordonNode(c Controller, node *corev1.Node) error {
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.TODO(), 5*time.Second)
 	defer cancel()
 
 	drainer := &drain.Helper{
