e802094f2edbe2f49d199503dd7bc9c4c8909d42
shishir9159 <nahidhasan43@iut-dhaka.edu>
2024-11-01 03:11:43 +0600

    making minor-upgrade work across all nodes - broken - last dance

--- cmd/kapetanios/prerequisite.go
+++ cmd/kapetanios/prerequisite.go
@@ -23,7 +23,7 @@ func Prerequisites(namespace string) {
 	defer func(logger *zap.Logger) {
 		er := logger.Sync()
 		if er != nil {
-			logger.Fatal("error syncing logger before application terminates", zap.Error(err))
+			logger.Info("error syncing logger before application terminates", zap.Error(err))
 		}
 	}(logger)
 
