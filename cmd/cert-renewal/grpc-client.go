0f487ef09a51873cbc7821e8b380d9ceefdfe924
shishir9159 <nahidhasan43@iut-dhaka.edu>
2024-11-04 00:19:33 +0600

    cert renewal before breaking apart

--- cmd/cert-renewal/grpc-client.go
+++ cmd/cert-renewal/grpc-client.go
@@ -1,63 +1,11 @@
 package main
 
 import (
-	"context"
-	"flag"
-	pb "github.com/shishir9159/kapetanios/proto"
 	"go.uber.org/zap"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"time"
-)
-
-const (
-	defaultName = "cert-renewal"
-)
-
-var (
-	addr = flag.String("addr", "kapetanios.default.svc.cluster.local:50051", "the address to connect to")
-	//addr = flag.String("addr", "dns:[//10.96.0.1/]kapetanios.default.svc.cluster.local[:50051]", "the address to connect to")
-	name = flag.String("name", defaultName, "gRPC test")
 )
 
 func GrpcClient(log *zap.Logger) {
 
-	flag.Parse()
-
-	// Set up a connection to the server.
-	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Error("did not connect", zap.Error(err))
-	}
-	//grpc.WithDisableServiceConfig()
-	defer func(conn *grpc.ClientConn) {
-		er := conn.Close()
-		if er != nil {
-			log.Error("failed to close the grpc connection",
-				zap.Error(er))
-		}
-	}(conn)
-
-	c := pb.NewRenewalClient(conn)
-
-	// Contact the server and print out its response.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
 	// TODO: refactor with renewal in the proto definitions
-	rpc, err := c.StatusUpdate(ctx,
-		&pb.CreateRequest{
-			BackupSuccess:  true,
-			RenewalSuccess: true,
-			RetryAttempt:   0,
-			RestartSuccess: true,
-			Log:            "",
-			Err:            "",
-		})
 
-	if err != nil {
-		log.Error("could not send status update: ", zap.Error(err))
-	}
-
-	log.Info("Status Update", zap.Bool("next step", rpc.GetProceedNextStep()), zap.Bool("retry", rpc.GetSkipRetryCurrentStep()))
 }
--- cmd/cert-renewal/main.go
+++ cmd/cert-renewal/main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"github.com/gofiber/fiber/v2/log"
+	pb "github.com/shishir9159/kapetanios/proto"
 	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"time"
 )
+
+const (
+	defaultName = "cert-renewal"
+)
+
+var ()
 
 var (
 	backupCount            = 7
 	overRideUserKubeConfig = 0
+	name                   = flag.String("name", defaultName, "gRPC test")
+	//addr = flag.String("addr", "dns:[//10.96.0.1/]kapetanios.default.svc.cluster.local[:50051]", "the address to connect to")
+	addr = flag.String("addr", "kapetanios.default.svc.cluster.local:50051", "the address to connect to")
 )
 
 type Controller struct {
@@ -39,6 +54,43 @@ func main() {
 				zap.Error(er))
 		}
 	}(logger)
+
+	flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Error("did not connect", zap.Error(err))
+	}
+	//grpc.WithDisableServiceConfig()
+	defer func(conn *grpc.ClientConn) {
+		er := conn.Close()
+		if er != nil {
+			log.Error("failed to close the grpc connection",
+				zap.Error(er))
+		}
+	}(conn)
+
+	connection := pb.NewRenewalClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	rpc, err := connection.StatusUpdate(ctx,
+		&pb.CreateRequest{
+			BackupSuccess:  true,
+			RenewalSuccess: true,
+			RetryAttempt:   0,
+			RestartSuccess: true,
+			Log:            "",
+			Err:            "",
+		})
+
+	if err != nil {
+		log.Error("could not send status update: ", zap.Error(err))
+	}
+
+	log.Info("Status Update", zap.Bool("next step", rpc.GetProceedNextStep()), zap.Bool("retry", rpc.GetSkipRetryCurrentStep()))
 
 	err = PrerequisitesForCertRenewal(c.log)
 	if err != nil {
