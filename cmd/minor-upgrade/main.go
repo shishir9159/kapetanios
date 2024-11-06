92450f76edb24114114c8e2affa9f2ff5ea50694
shishir9159 <nahidhasan43@iut-dhaka.edu>
2024-11-06 16:57:44 +0600

    REMOVED GRPC DNS RESOLVER

--- cmd/minor-upgrade/main.go
+++ cmd/minor-upgrade/main.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	pb "github.com/shishir9159/kapetanios/proto"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"net"
 	"time"
 	//"github.com/rs/zerolog/log"
 )
@@ -20,88 +18,6 @@ var (
 type Controller struct {
 	ctx context.Context
 	log *zap.Logger
-}
-
-func GrpcClient(log *zap.Logger) {
-
-	var addr *string
-
-	r := &net.Resolver{
-		PreferGo: true,
-		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
-			d := net.Dialer{
-				Timeout: time.Millisecond * time.Duration(10000),
-			}
-			return d.DialContext(ctx, network, "10.96.0.10:53")
-		},
-	}
-
-	ips, err := r.LookupNetIP(context.Background(), "ip4", "kapetanios.default.svc.cluster.local")
-	log.Info("kapetanios service address")
-	fmt.Println(ips)
-
-	if len(ips) != 0 {
-		for i, ip := range ips {
-			if ip.BitLen() == 32 {
-				log.Info("kapetanios service address", zap.String("ip", ip.String()))
-				addr = flag.String("addr", ip.String()+":50051", "the address to connect to")
-				log.Info("address", zap.Int("index", i), zap.String("addr", *addr))
-			}
-		}
-	}
-
-	if err != nil {
-		log.Error("error kapetanios address", zap.Error(err))
-	}
-
-	if addr == nil {
-		log.Error("addr was empty")
-		addr = flag.String("addr", "kapetanios.default.svc.cluster.local:50051", "the address to connect to")
-	}
-
-	flag.Parse()
-
-	log.Info("connecting to kapetanios service address", zap.String("addr", *addr))
-
-	log.Info("1")
-	// Set up a connection to the server.
-	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Error("did not connect", zap.Error(err))
-	}
-	log.Info("2")
-	//grpc.WithDisableServiceConfig()
-	defer func(conn *grpc.ClientConn) {
-		er := conn.Close()
-		if er != nil {
-			log.Error("failed to close the grpc connection",
-				zap.Error(er))
-		}
-	}(conn)
-
-	log.Info("3")
-	c := pb.NewMinorUpgradeClient(conn)
-
-	// Contact the server and print out its response.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	log.Info("4")
-	defer cancel()
-
-	rpc, err := c.ClusterHealthChecking(ctx,
-		&pb.PrerequisitesMinorUpgrade{
-			EtcdStatus:          false,
-			StorageAvailability: 0,
-			Err:                 "",
-		})
-
-	if err != nil {
-		log.Error("could not send status update: ", zap.Error(err))
-	}
-
-	log.Info("Backup Status",
-		zap.Bool("next step", rpc.GetProceedNextStep()),
-		zap.Bool("retry", rpc.GetSkipRetryCurrentStep()),
-		zap.Bool("terminate application", rpc.GetTerminateApplication()))
 }
 
 func main() {
