473a30dd47a5c42b2b8cebc79369c826474dd6fa
shishir9159 <nahidhasan43@iut-dhaka.edu>
2024-11-07 21:16:32 +0600

    expiration started -

--- cmd/kapetanios/cert-expiration.go
+++ cmd/kapetanios/cert-expiration.go
@@ -1,1 +1,94 @@
 package main
+
+import (
+	"context"
+	"github.com/shishir9159/kapetanios/internal/orchestration"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"time"
+)
+
+func Expiration(namespace string) {
+
+	logger, err := zap.NewProduction()
+	defer func(logger *zap.Logger) {
+		er := logger.Sync()
+		if er != nil {
+			logger.Fatal("error syncing logger before application terminates", zap.Error(err))
+		}
+	}(logger)
+
+	// TODO:
+	//  refactor
+	client, err := orchestration.NewClient()
+
+	c := Controller{
+		client:    client,
+		ctx:       context.Background(),
+		log:       logger,
+		namespace: namespace,
+	}
+
+	if err != nil {
+		c.log.Error("error creating kubernetes client",
+			zap.Error(err))
+	}
+
+	roleName := "certs"
+	matchLabels := map[string]string{"assigned-node-role-certs.kubernetes.io": roleName}
+
+	labelSelector := metav1.LabelSelector{MatchLabels: matchLabels}
+	listOptions := metav1.ListOptions{
+		LabelSelector: labels.Set(labelSelector.MatchLabels).String(),
+	}
+
+	renewalMinionManager := orchestration.NewMinions(client)
+
+	nodes, err := c.client.Clientset().CoreV1().Nodes().List(context.Background(), listOptions)
+	if err != nil {
+		c.log.Error("error listing nodes",
+			zap.Error(err))
+	}
+
+	if len(nodes.Items) == 0 {
+		c.log.Error("no master nodes found",
+			zap.Error(err))
+		//	return err or call grpc
+	}
+
+	// TODO: refactor this part inside the orchestrator
+
+	for index, node := range nodes.Items {
+
+		// namespace should only be included after the consideration for the existing
+		// service account, cluster role binding
+		descriptor := renewalMinionManager.MinionBlueprint("quay.io/klovercloud/certs-expiration", roleName, node.Name)
+
+		ch := make(chan *grpc.Server, 1)
+		go ExpirationGrpc(c.log, ch)
+
+		// kubectl get event --namespace default --field-selector involvedObject.name=minions
+		// how many pods this logic need to be in the orchestration too
+		minion, er := c.client.Clientset().CoreV1().Pods(namespace).Create(context.Background(), descriptor, metav1.CreateOptions{})
+		if er != nil {
+			c.log.Error("Cert Expiration pod creation failed: ",
+				zap.Int("index", index),
+				zap.Error(er))
+
+			// er
+			return
+		}
+
+		(<-ch).Stop()
+
+		c.log.Info("Cert Expiration pod created",
+			zap.Int("index", index),
+			zap.String("pod_name", minion.Name))
+
+		// todo: wait for request for restart from the minions
+		time.Sleep(3 * time.Second)
+	}
+
+}
