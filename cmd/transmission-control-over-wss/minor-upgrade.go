77a6c6605e705ea3d955208a9e5185023cf48972
shishir9159 <nahidhasan43@iut-dhaka.edu>
2025-02-09 10:04:00 +0600

    purged

--- cmd/transmission-control-over-wss/minor-upgrade.go
+++ cmd/transmission-control-over-wss/minor-upgrade.go
@@ -12,10 +12,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/kubectl/pkg/drain"
-	"os"
 	"strconv"
-	"strings"
 	"sync"
 	"time"
 )
@@ -31,93 +28,6 @@ type Controller struct {
 	namespace string
 	ctx       context.Context
 	log       *zap.Logger
-}
-
-func drainAndCordonNode(c Controller, node *corev1.Node) error {
-
-	drainer := &drain.Helper{
-		Ctx:                             c.ctx,
-		Client:                          c.client.Clientset(),
-		DisableEviction:                 true,
-		Force:                           true, // TODO: should it be Force eviction?
-		IgnoreAllDaemonSets:             true,
-		DeleteEmptyDirData:              true,
-		SkipWaitForDeleteTimeoutSeconds: 30,
-		Timeout:                         3 * time.Minute,
-		GracePeriodSeconds:              10,
-		Out:                             os.Stdout,
-		ErrOut:                          os.Stderr,
-	}
-
-	err := drain.RunCordonOrUncordon(drainer, node, true)
-	if err != nil {
-		c.log.Error("error cordoning node",
-			zap.String("node", node.Name),
-			zap.Error(err))
-
-	}
-
-	err = drain.RunNodeDrain(drainer, node.Name)
-	if err != nil {
-		c.log.Error("error draining node",
-			zap.String("node", node.Name),
-			zap.Error(err))
-	}
-
-	return nil
-}
-
-func removeTaint(node *corev1.Node) {
-
-	taints := node.Spec.Taints
-
-	if len(taints) == 0 {
-		return
-	}
-
-	taintToRemove := corev1.Taint{
-		Key:    "minor-upgrade-running",
-		Value:  "processing",
-		Effect: corev1.TaintEffectNoSchedule,
-	}
-
-	var newTaints []corev1.Taint
-
-	for _, taint := range taints {
-		if taint.MatchTaint(&taintToRemove) {
-			continue
-		}
-		newTaints = append(newTaints, taint)
-	}
-
-	node.Spec.Taints = newTaints
-}
-
-func addTaint(node *corev1.Node) {
-
-	taints := node.Spec.Taints
-
-	taintToAdd := corev1.Taint{
-		Key:    "minor-upgrade-running",
-		Value:  "processing",
-		Effect: corev1.TaintEffectNoSchedule,
-	}
-
-	newTaints := []corev1.Taint{taintToAdd}
-
-	if len(taints) != 0 {
-		for _, taint := range taints {
-			if taint.MatchTaint(&taintToAdd) {
-				return
-			}
-
-			newTaints = append(newTaints, taint)
-		}
-
-		return
-	}
-
-	node.Spec.Taints = newTaints
 }
 
 func recovery(namespace string) {
@@ -239,32 +149,6 @@ func MinorUpgradeFirstRun(namespace string, conn *websocket.Conn) {
 	descriptor.Spec.Containers[0].Env = env
 	descriptor.Spec.DNSPolicy = corev1.DNSClusterFirstWithHostNet
 
-	c.log.Info("cordoning and draining node",
-		zap.String("node name", node.Name))
-
-	err = drainAndCordonNode(c, &node)
-	if err != nil {
-		c.log.Error("failed to drain node",
-			zap.String("node name:", node.Name),
-			zap.Error(err))
-	}
-
-	c.log.Info("tainting node",
-		zap.String("node name", node.Name))
-
-	addTaint(&node)
-
-	err = drain.RunCordonOrUncordon(&drain.Helper{
-		Ctx:    c.ctx,
-		Client: c.client.Clientset(),
-	}, &node, false)
-
-	if err != nil {
-		c.log.Error("error uncordoning the node",
-			zap.String("node name", node.Name),
-			zap.Error(err))
-	}
-
 	ch := make(chan *grpc.Server, 1)
 	go MinorUpgradeGrpc(c.log, conn, ch)
 
@@ -303,99 +187,4 @@ func MinorUpgradeFirstRun(namespace string, conn *websocket.Conn) {
 	time.Sleep(50 * time.Second)
 
 	(<-ch).Stop()
-
-	removeTaint(&node)
-}
-
-func LastDance(c Controller, nodes string, namespace string) {
-
-	renewalMinionManager := orchestration.NewMinions(c.client)
-
-	roleName := "minor-upgrade"
-
-	nodeList := strings.Split(nodes, ";")
-
-	for index, node := range nodeList {
-
-		descriptor := renewalMinionManager.MinionBlueprint("quay.io/klovercloud/minor-upgrade", roleName, node)
-
-		descriptor.Spec.Tolerations = []corev1.Toleration{
-			{
-				Key:      "minor-upgrade-running",
-				Operator: "Equal",
-				Value:    "processing",
-				Effect:   corev1.TaintEffectNoSchedule,
-			},
-		}
-
-		descriptor.Spec.HostNetwork = true
-
-		certificateRenewal := false
-
-		certRenewalEnv := corev1.EnvVar{
-			Name:  "CERTIFICATE_RENEWAL",
-			Value: strconv.FormatBool(certificateRenewal),
-		}
-
-		env := descriptor.Spec.Containers[0].Env
-		env = append(env, certRenewalEnv)
-		descriptor.Spec.Containers[0].Env = env
-		descriptor.Spec.DNSPolicy = corev1.DNSClusterFirstWithHostNet
-
-		no, err := c.client.Clientset().CoreV1().Nodes().Get(c.ctx, node, metav1.GetOptions{})
-		if err != nil {
-			c.log.Error("failed to get node by node name",
-				zap.String("node name:", node),
-				zap.Error(err))
-		}
-
-		c.log.Info("cordoning and draining node",
-			zap.String("node name", node))
-
-		err = drainAndCordonNode(c, no)
-		if err != nil {
-			c.log.Error("failed to drain node",
-				zap.String("node name:", node),
-				zap.Error(err))
-		}
-
-		c.log.Info("tainting node",
-			zap.String("node name", node))
-
-		addTaint(no)
-
-		err = drain.RunCordonOrUncordon(&drain.Helper{
-			Ctx:    c.ctx,
-			Client: c.client.Clientset(),
-		}, no, false)
-
-		if err != nil {
-			c.log.Error("error uncordoning the node",
-				zap.String("node name", node),
-				zap.Error(err))
-		}
-
-		ch := make(chan *grpc.Server, 1)
-		//TODO:
-		//  go MinorUpgradeGrpc(c.log, ch)
-
-		minion, er := c.client.Clientset().CoreV1().Pods(namespace).Create(context.Background(), descriptor, metav1.CreateOptions{})
-		if er != nil {
-			c.log.Error("minor upgrade pod creation failed: ",
-				zap.Int("index", index),
-				zap.Error(er))
-
-			return
-		}
-
-		c.log.Info("minor upgrade pod created",
-			zap.Int("index", index),
-			zap.String("pod name", minion.Name))
-
-		time.Sleep(50 * time.Second)
-
-		(<-ch).Stop()
-
-		removeTaint(no)
-	}
 }
